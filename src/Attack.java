486f2c38049f1844bb5ef5608ed64474b5a0960c
Vasily Osipov <vpo5039@psu.edu>
2023-02-19 21:49:35 -0500

    Added the actions classes.

---
+++ src/Attack.java
@@ -0,0 +1,5 @@
+public class Attack extends Action{
+    public Attack(){
+        super(Method.Attack, "Attack", 'a', null);
+    }
+}
---
+++ src/Method.java
@@ -0,0 +1,3 @@
+public enum Method {
+    Flee, Attack, ViewInventory, MoveEast, MoveWest, MoveSouth, MoveNorth
+}
