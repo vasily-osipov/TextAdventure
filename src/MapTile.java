4cfdb751d53428998f440db8c8112226c519f658
Vasily Osipov <vpo5039@psu.edu>
2023-03-12 22:07:08 -0400

    Added World and MapTile classes

---
+++ src/MapTile.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class MapTile {
+    int x;
+    int y;
+
+    public MapTile(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public ArrayList<Action> adjacent_moves(){
+        ArrayList<Action> moves = new ArrayList<Action>();
+        if(World.tile_exists(x, y+1) != null){
+            moves.add(new MoveEast());
+        }
+        if(World.tile_exists(x, y-1) != null){
+            moves.add(new MoveWest());
+        }
+        if(World.tile_exists(x-1, y) != null){
+            moves.add(new MoveNorth());
+        }
+        if(World.tile_exists(x+1, y) != null){
+            moves.add(new MoveSouth());
+        }
+        return moves;
+    }
+
+}
